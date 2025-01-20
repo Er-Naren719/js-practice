0933a1b5b3a44dac527e1b599c6ceac303febe76
Naren <49683121+Er-Naren719@users.noreply.github.com>
2025-01-20 10:24:26 +0000

    let, const and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 54;
+let accountEmail = "narendra.patil@ayekart.com";
+var accountPasskey = "ahsdkj";
+location = "Pune";
+let state;
+
+// accountId = 45; // This is not allowed
+accountEmail = "np@np.com"
+accountPasskey = "asd"
+location = "Mumbai"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPasskey, location, state])
