434ddf9877cbb8e86c1f7391d2059fd101adf9d1
Naren <49683121+Er-Naren719@users.noreply.github.com>
2025-01-21 13:00:20 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = true;
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// How Number conversion happens
+// "33" => 33
+// "33abc" => NaN
+// true/false => 1/0
+
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// How Boolean conversion happens
+// 1 => true
+// 0 => false
+// "naren" => true
+// "" => false
+
+
+let someNumber = 12;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
