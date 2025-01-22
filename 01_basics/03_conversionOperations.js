c7b7ba0e5927c3784cda3df98e4e6e6ab917ce0b
Naren <49683121+Er-Naren719@users.noreply.github.com>
2025-01-22 09:43:44 +0000

    comparisons

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,5 +24,25 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 12;
 let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+
+// ************************************************* Operations *************************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " naren";
+// console.log(str1+str2);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4)*5%3);
+
+console.log(+true);
+console.log(+"");
