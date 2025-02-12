e64726b840fca9dff64de69d6b119e2ed199f40f
Naren <49683121+Er-Naren719@users.noreply.github.com>
2025-02-12 06:09:44 +0000

    objects methods

---
+++ 02_basics/04_singleton_objects.js
@@ -0,0 +1,46 @@
+// const partUser = new Object();
+
+const partUser = {};
+partUser.id = "123abc";
+partUser.name = "Sam";
+partUser.isLoggedIn = true;
+// console.log(partUser);
+
+const regularUser = {
+    email: "fake@dummy.com",
+    fullName: {
+        userFullName: {
+            firstName: "Naren",
+            lastName: "Patil"
+        }
+    }
+}
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        name: "a"
+    },
+    {
+        id: 2,
+        name: "b"
+    },
+    {
+        id: 3,
+        name: "c"
+    },
+]
+// console.log(users[1].name);
+// console.log(Object.keys(partUser));
+// console.log(Object.values(partUser));
+// console.log(Object.entries(partUser));
+
+console.log(partUser.hasOwnProperty("name"));
