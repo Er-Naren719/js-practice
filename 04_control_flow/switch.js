76b32a9f13945ff9fa6b36cfbc9144c163e9a40f
Naren <49683121+Er-Naren719@users.noreply.github.com>
2025-03-25 05:18:16 +0000

    switch condition

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,15 @@
+const month = 4;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    default:
+        console.log("Remaining months");
+}
