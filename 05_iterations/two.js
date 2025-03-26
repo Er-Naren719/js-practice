3d5176093922ddc41962eb2698a9d6fc2ff1c704
Naren <49683121+Er-Naren719@users.noreply.github.com>
2025-03-26 06:17:45 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// While loop
+let myArray = ["a", "b", "c"];
+let newArr = 0;
+while (newArr < myArray.length) {
+    // console.log(myArray[newArr]);
+    newArr = newArr + 1;
+}
+
+// Do - While loop
+let score = 11;
+do {
+    console.log(`value of score is ${score}`);
+    score++;
+} while (score <= 10);
